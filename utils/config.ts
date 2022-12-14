17055af4d6042c20f42506c95a381f81436f8687
MonsterPi13 <rpishappynow@gmail.com>
2022-12-14 19:55:28 +0800

    fix: production mode can't get env variables

--- utils/config.ts
+++ utils/config.ts
@@ -2,18 +2,22 @@ import EnvNames from "../constants/EnvVars.ts";
 import { config as dotEnvConfig } from "dotEnv";
 import { Config, ConfigSchema } from "@/schemas/Config.ts";
 
-const env = await dotEnvConfig();
+if (Deno.env.get(EnvNames.DENO_ENV) !== "production") {
+  await dotEnvConfig({
+    export: true,
+  });
+}
 
 const envConfig: Config = {
-  base_url: env[EnvNames.BASE_URL] || "",
-  environment: env[EnvNames.DENO_ENV] || "",
+  base_url: Deno.env.get(EnvNames.BASE_URL) || "",
+  environment: Deno.env.get(EnvNames.DENO_ENV) || "",
   oauth: {
     github: {
-      client_id: env[EnvNames.GITHUB_CLIENT_ID] || "",
-      client_secret: env[EnvNames.GITHUB_CLIENT_SECRET] || "",
+      client_id: Deno.env.get(EnvNames.GITHUB_CLIENT_ID) || "",
+      client_secret: Deno.env.get(EnvNames.GITHUB_CLIENT_SECRET) || "",
     },
   },
-  DATABASE_URL: env[EnvNames.DATABASE_URL] || "",
+  DATABASE_URL: Deno.env.get(EnvNames.DATABASE_URL) || "",
 };
 
 const config = ConfigSchema.parse(envConfig);
